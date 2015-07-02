c3cc6dd3f923d1aab5aa925ed8522357a104c795
Josh Betz <j@joshbetz.com>
2015-07-02 17:03:27 -0500

    Change priority of filters to 9999
    This should always, always be last. We want to avoid accidentally disabling with another plugin.
    
    props @nickdaugherty

--- jetpack-force-2fa.php
+++ jetpack-force-2fa.php
@@ -9,19 +9,19 @@ Author URI: http://automattic.com
 */
 
 // Hide the login form
-add_filter('jetpack_remove_login_form', '__return_true');
-add_filter('jetpack_sso_bypass_login_forward_wpcom', '__return_true');
-add_filter('jetpack_sso_display_disclaimer', '__return_false');
+add_filter('jetpack_remove_login_form', '__return_true', 9999);
+add_filter('jetpack_sso_bypass_login_forward_wpcom', '__return_true', 9999);
+add_filter('jetpack_sso_display_disclaimer', '__return_false', 9999);
 
 // Force users to use the same email address as WPCOM
-add_filter('jetpack_sso_match_by_email', '__return_true');
+add_filter('jetpack_sso_match_by_email', '__return_true', 9999);
 
 // Completely disable the standard login form
 add_filter('wp_authenticate_user', function() {
 	return new WP_Error('nope', "You ain't logging in here.");
-});
+}, 9999);
 
 // Force 2FA for admins
 add_filter('jetpack_sso_require_two_step', function() {
 	return current_user_can('manage_options');
-});
+}, 9999);
