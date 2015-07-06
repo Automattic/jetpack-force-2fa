fab547d328c78e3934ebf52c80f267fa314854cf
Brandon Kraft <public@brandonkraft.com>
2015-07-06 09:18:25 -0500

    Force 2fa for all users on a multisite as a temporary hack for now. See #5

--- jetpack-force-2fa.php
+++ jetpack-force-2fa.php
@@ -8,23 +8,35 @@ Version: 0.1
 Author URI: http://automattic.com
 */
 
-// Hide the login form
-/* Disabling via a comment so it is trivial to revert if not ready for primetime by deadline.
-add_filter('jetpack_remove_login_form', '__return_true', 9999);
-add_filter('jetpack_sso_bypass_login_forward_wpcom', '__return_true', 9999);
-add_filter('jetpack_sso_display_disclaimer', '__return_false', 9999);
-*/
-
 // Allows WP.com login to a local account if it matches the local account.
 add_filter('jetpack_sso_match_by_email', '__return_true', 9999);
 
-// Completely disable the standard login form for admins.
-add_filter('wp_authenticate_user', function( $user ) {
-	if ( $user->has_cap('manage_options') ) {
-		return new WP_Error('wpcom-required', "Local login disabled for this account.", $user->user_login );
-	}
-	return $user;
-}, 9999);
+if ( is_multisite() ) { // Temporary for multisites until admin-only option developed.
+
+	// Hide the login form
+	add_filter('jetpack_remove_login_form', '__return_true', 9999);
+	add_filter('jetpack_sso_bypass_login_forward_wpcom', '__return_true', 9999);
+	add_filter('jetpack_sso_display_disclaimer', '__return_false', 9999);
+
+	add_filter( 'wp_authenticate_user', function() {
+		return new WP_Error( 'wpcom-required', "Local login disabled for this site.");
+	}, 9999);
+
+	add_filter('jetpack_sso_require_two_step', '__return_true');
+
+} // end if multisite
+
+else { // not multisite
+	// Completely disable the standard login form for admins.
+	add_filter('wp_authenticate_user', function( $user ) {
+		if ( $user->has_cap('manage_options') ) {
+			return new WP_Error('wpcom-required', "Local login disabled for this account.", $user->user_login );
+		}
+		return $user;
+	}, 9999);
+
+	add_action( 'jetpack_sso_pre_handle_login', 'jetpack_set_two_step_for_admins' );
+} // end multisite else
 
 function jetpack_set_two_step_for_admins( $user_data ){
 	$user = bk_temp_get_user_by_wpcom_id( $user_data->ID );
@@ -38,8 +50,6 @@ function jetpack_set_two_step_for_admins( $user_data ){
 		add_filter('jetpack_sso_require_two_step', '__return_true');
 	}
 }
-
-add_action( 'jetpack_sso_pre_handle_login', 'jetpack_set_two_step_for_admins' );
 
 // Lifted this wholly from Jetpack since it is a non-static function. Idael would be make it so in JP
 function bk_temp_get_user_by_wpcom_id( $wpcom_user_id ){
