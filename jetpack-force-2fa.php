9728978af7662a29dca395a3065cae9e26fdc496
Brandon Kraft <public@brandonkraft.com>
2015-07-19 09:56:16 -0400

    Code Standards: Whitespacing.

--- jetpack-force-2fa.php
+++ jetpack-force-2fa.php
@@ -9,18 +9,20 @@ Author URI: http://automattic.com
 */
 
 // Allows WP.com login to a local account if it matches the local account.
-add_filter('jetpack_sso_match_by_email', '__return_true', 9999);
+add_filter( 'jetpack_sso_match_by_email', '__return_true', 9999 );
 
 if ( is_multisite() ) { // Temporary for multisites until admin-only option developed.
 
 	// Hide the login form
-	add_filter('jetpack_remove_login_form', '__return_true', 9999);
-	add_filter('jetpack_sso_bypass_login_forward_wpcom', '__return_true', 9999);
-	add_filter('jetpack_sso_display_disclaimer', '__return_false', 9999);
+	add_filter( 'jetpack_remove_login_form', '__return_true', 9999 );
+	add_filter( 'jetpack_sso_bypass_login_forward_wpcom', '__return_true', 9999 );
+	add_filter( 'jetpack_sso_display_disclaimer', '__return_false', 9999 );
 
 	add_filter( 'wp_authenticate_user', function() {
-		return new WP_Error( 'wpcom-required', "Local login disabled for this site.");
-	}, 9999);
+		return new WP_Error( 'wpcom-required', "Local login disabled for this site." );
+	}, 9999 );
+
+	add_filter( 'jetpack_sso_require_two_step', '__return_true' );
 
 	add_filter( 'allow_password_reset', '__return_false' );
 
@@ -28,9 +30,9 @@ if ( is_multisite() ) { // Temporary for multisites until admin-only option deve
 
 else { // not multisite
 	// Completely disable the standard login form for admins.
-	add_filter('wp_authenticate_user', function( $user ) {
+	add_filter( 'wp_authenticate_user', function( $user ) {
 		if ( $user->has_cap('manage_options') ) {
-			return new WP_Error('wpcom-required', "Local login disabled for this account.", $user->user_login );
+			return new WP_Error( 'wpcom-required', "Local login disabled for this account.", $user->user_login );
 		}
 		return $user;
 	}, 9999);
